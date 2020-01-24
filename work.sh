1b79809bc00933bdd264396a922790a8187102df
ashokpillai <45928403+ashokpillai@users.noreply.github.com>
2020-01-24 17:14:55 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -13,12 +13,6 @@ mkdir revproui
 unzip revproui.zip -d revproui
 cd revproui
 
-#aws s3 cp . s3://release.revpro.cloud/master/$VERSION_NUM/ --recursive --acl public-read
+aws s3 cp . s3://release.revpro.cloud/master/$VERSION_NUM/ --recursive --acl public-read
 
-if [[ $? -ne 0 ]]
-then
-echo "Script Ends with error "
-else
-echo "success"
-fi
 
